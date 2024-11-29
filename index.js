ae29a546d6677d8f31dfaccd526a06a4cf66f874
Tanyachitsa <165687341+Tanyachitsa@users.noreply.github.com>
2024-11-29 11:32:58 +0200

    added to index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+let button = document.querySelector(".top-bar.weather");
+button.addEventListener("click", function () {
+  window.open("https://finalweatherprojectshecodes.netlify.app/", "_blank");
+});
+
+let buttonRecipe = document.querySelector(".top-bar.recipe");
+buttonRecipe.addEventListener("click", function () {
+  window.open("pythonProject.html", "_blank");
+});
